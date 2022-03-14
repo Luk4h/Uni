3173205ac874084c291bd60349b2367efe007979
Luiz Gustavo Marcondes <luizgustavodevergennes@gmail.com>
2022-03-14 20:58:49 -0300

    Discovering changes in sizes by using pointers

--- Estruturas de Dados/Ponteiros e alocação dinâmica/main.cpp
+++ Estruturas de Dados/Ponteiros e alocação dinâmica/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+// Normal struct with every type of data.
+struct Tdado {
+    int a;
+    float b;
+    double c;
+    char d;
+    bool e;
+};
+
+// Struct with every type of pointer.
+struct Tdado_ptr {
+    int* a;
+    float* b;
+    double* c;
+    char* d;
+    bool* e;
+};
+
 int main(){
-    std::cout<<"Hello World";
+    std::cout<<"Size of TDado: "<< sizeof(Tdado) << std::endl;
+    std::cout<<"Size of TDado_ptr: "<< sizeof(Tdado_ptr) << std::endl;
+    std::cout<<"Size of TDado*: "<< sizeof(Tdado*) << std::endl;
+    std::cout<<"Size of TDado_ptr*: "<< sizeof(Tdado_ptr*) << std::endl;
 }
