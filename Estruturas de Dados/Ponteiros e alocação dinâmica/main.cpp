e32b4e506da27831901c5c53ab0f89afb2162adc
Luiz Gustavo Marcondes <luizgustavodevergennes@gmail.com>
2022-03-14 21:43:42 -0300

    Syntax beautification.

--- Estruturas de Dados/Ponteiros e alocação dinâmica/main.cpp
+++ Estruturas de Dados/Ponteiros e alocação dinâmica/main.cpp
@@ -24,31 +24,31 @@ int main(){
 
     // Size of the struct assigned and pointer of struct assigned.
     Tdado a;
-    std::cout<<"Size of a: "<< a) << std::endl;
+    std::cout << "Size of a: " << sizeof(a) << std::endl;
     Tdado* b;
-    std::cout<<"Size of a: "<< b) << std::endl;
+    std::cout << "Size of a: " << sizeof(b) << std::endl;
 
     // Size of struct and struct with pointers.
-    std::cout<<"Size of TDado: "<< Tdado) << std::endl;
-    std::cout<<"Size of TDado_ptr: "<< Tdado_ptr) << std::endl;
+    std::cout << "Size of TDado: " << sizeof(Tdado) << std::endl;
+    std::cout << "Size of TDado_ptr: " << sizeof(Tdado_ptr) << std::endl;
 
     // Pointers always have 4 bytes of size.
-    std::cout<<"Size of TDado*: "<< Tdado*) << std::endl;
-    std::cout<<"Size of TDado_ptr*: "<< Tdado_ptr*) << std::endl;
+    std::cout << "Size of TDado*: " << sizeof(Tdado*) << std::endl;
+    std::cout << "Size of TDado_ptr*: " << sizeof(Tdado_ptr*) << std::endl;
 
     */
 
    /*
 
     int a = 10;
-    std::cout<<"Size of a: "<< a <<" - Size of a&: "<< &a << std::endl;
+    std::cout << "Size of a: " << a << " - Size of a&: " << &a << std::endl;
 
     int* p_a = &a;
-    std::cout<<"Size of p_a: "<< p_a <<" - Size of p_a&: "<< &p_a << std::endl;
+    std::cout << "Size of p_a: " << p_a << " - Size of p_a&: " << &p_a << std::endl;
 
     // Dynamic allocation.
     int* p_new_a = new int[5];
-    std::cout<<"Size of p_new_a: "<< p_new_a <<" - Size of p_new_a&: "<< &p_new_a << std::endl;
+    std::cout << "Size of p_new_a: " << p_new_a << " - Size of p_new_a&: " << &p_new_a << std::endl;
    
    */
 
@@ -59,16 +59,16 @@ int main(){
     std::cin>>a;
 
     int* b = new int[a];
-    std::cout<<"Size of a: "<< a) << std::endl;
+    std::cout << "Size of a: " << a) << std::endl;
 
     */
 
    int a = 10;
    int* p_a = &a;
 
-   std::cout<<"Valor: "<< p_a << std::endl;
-   std::cout<<"Endereço: "<< &p_a << std::endl;
-   std::cout<<"Conteudo do endereço: "<< *p_a << std::endl;
+   std::cout << "Valor: " << p_a << std::endl;
+   std::cout << "Endereço: " << &p_a << std::endl;
+   std::cout << "Conteudo do endereço: " << *p_a << std::endl;
 
    return 0;
 }
